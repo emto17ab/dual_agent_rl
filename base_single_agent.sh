0cf7eedc6327b113ecbae4db138cfa14ac4c3407
emto17ab <52211409+emto17ab@users.noreply.github.com>
2026-02-16 15:26:03 +0100

    Add files via upload

---
+++ base_single_agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -q hpc
+#BSUB -J "single_agent_nyc_man_south_v3[2]"
+#BSUB -n 8
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -W 72:00
+#BSUB -o logs/single_agent_nyc_man_south_v3_mode%I_%J.out
+#BSUB -e logs/single_agent_nyc_man_south_v3_mode%I_%J.err
+
+source /work3/s233791/rl-pricing-amod/thesis_env/bin/activate
+
+# Subtract 1 from job index to get mode 0-2
+MODE=$((LSB_JOBINDEX - 1))
+
+python main_a2c.py --reward_scalar 2000 --critic_warmup_episodes 50 --mode ${MODE} --city "nyc_man_south" --q_lr 0.0006 --p_lr 0.0002 --actor_clip 1000 --critic_clip 1000 --max_episodes 400000 --observe_od_prices --checkpoint_path single_agent_nyc_man_south_v3_mode${MODE}
---
+++ vehicle_split_experiment.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#BSUB -q hpc
+#BSUB -J "dual_agent_nyc_man_south_vehicle_split[1-4]"
+#BSUB -n 14
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -W 72:00
+#BSUB -o logs/dual_agent_nyc_man_south_vehicle_split_ratio%I_%J.out
+#BSUB -e logs/dual_agent_nyc_man_south_vehicle_split_ratio%I_%J.err
+
+source /work3/s233791/rl-pricing-amod/thesis_env/bin/activate
+
+# Map job index to agent0_vehicle_ratio
+case $LSB_JOBINDEX in
+    1)
+        RATIO=0.1
+        ;;
+    2)
+        RATIO=0.2
+        ;;
+    3)
+        RATIO=0.3
+        ;;
+    4)
+        RATIO=0.4
+        ;;
+esac
+
+# Always use mode 2
+MODE=2
+
+python main_a2c_multi_agent.py --load --reward_scalar 5000 --critic_warmup_episodes 0 --mode $MODE --city "nyc_man_south" --q_lr 0.0006 --p_lr 0.0002 --actor_clip 1000 --critic_clip 1000 --max_episodes 100000 --observe_od_prices --agent0_vehicle_ratio $RATIO --checkpoint_path dual_agent_nyc_man_south_vehicle_split_ratio${RATIO}_mode${MODE}
